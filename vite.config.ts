6ba1bc19c8cdec648a472e1557df9f94f4e88302
Maxim <yungyoku@yandex.ru>
2023-04-11 10:34:37 +0300

    Edits

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import stylelint from "vite-plugin-stylelint";
 import { VitePWA } from "vite-plugin-pwa";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     stylelint(),
+    eslint(),
     VitePWA({
       srcDir: "public",
       filename: "sw.js",
